21ea40decc71794d94e198b1d1ee5a9c81be62ab
Prem Prakash Shinde <shindeprem102@gmail.com>
2023-09-26 20:34:04 +0530

    Done Some Examples On Bit Manipulation

---
+++ ClearithBit.java
@@ -0,0 +1,13 @@
+
+public class ClearithBit {
+
+    public static int ClearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ClearIthBit(10, 1));
+
+    }
+}
---
+++ UpdateIthBit.java
@@ -0,0 +1,27 @@
+public class UpdateIthBit {
+    public static int ClearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
+    public static int setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        return n | bitMask;
+    }
+
+    public static int updateIthBit(int n, int i, int newBit) {
+        // if (newBit == 0) {
+        // return ClearIthBit(n, i);
+        // } else {
+        // return setIthBit(n, newBit);
+        // }
+        n = ClearIthBit(n, i);
+        int bitMask = newBit << i;
+        return n | bitMask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(updateIthBit(10, 1, 3));
+
+    }
+}
