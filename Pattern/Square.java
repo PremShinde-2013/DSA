770ab0bd951bd3888c0f2702f26b7b3596ddf311
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-02-11 14:16:51 +0530

    added practice.md

--- Pattern/Square.java
+++ Pattern/Square.java
@@ -1,12 +1,32 @@
-class Square {
+// class Square {
+//     public static void main(String[] args) {
+//         // 5*5 box
+//         int n = 5;
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 System.out.print("*" + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+/**
+ * Square
+ */
+ class Square {
+
     public static void main(String[] args) {
-        // 5*5 box
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print("*" + " ");
+        int n = 6;
+     
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                System.out.print("*");
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
